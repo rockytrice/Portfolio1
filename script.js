09e6362292bba81a356ceff25cf6ffd79c17d153
Roderick Trice <rocky_trice@yahoo.com>
2018-10-06 01:29:05 -0400

    adding js code for background and fixed navbar. also added smooth scroll for the links when moving to different part of the page.

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    // navbar-toggler
+    $(".navbar-toggler").click(function () {
+        $(".navbar-toggler").toggleClass("change");
+    })
+
+// adding background and fixed navbar
+$(window).scroll(function(){
+    let position = $(this).scrollTop();
+        if(position >=149){
+            $(".navbar").addClass("navbar-background");
+            $(".navbar").addClass("fixed-top");
+        }
+        else{
+            $(".navbar").removeClass("navbar-background");
+            $(".navbar").removeClass("fixed-top");
+        }
+})
+    // smooth scroll
+    $(".nav-item a").click(function(link){
+        link.preventDefault();
+        let target = $(this).attr("href");
+        $("html,body").stop().animate({
+            scrollTop: $(target).offset().top
+        },3000);
+    })
+
+});
